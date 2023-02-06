f1f52b2e68f1229b1aced1988af496b1bdded5d8
Kivitagapeidus <S88rumail@gmail.com>
2023-02-06 16:43:54 +0200

    Esimene push

--- src/main/java/ee/valiit/back_quiz_valiit_project/BackQuizValiitProjectApplication.java
+++ src/main/java/ee/valiit/back_quiz_valiit_project/BackQuizValiitProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackQuizValiitProjectApplication {
 
+	//Eitea
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackQuizValiitProjectApplication.class, args);
 	}
