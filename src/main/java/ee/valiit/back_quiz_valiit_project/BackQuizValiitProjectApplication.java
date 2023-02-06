2696fdc062e74d2dc4800b6648ecc049582470ad
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-02-06 15:58:41 +0200

    Initial commit

---
+++ src/main/java/ee/valiit/back_quiz_valiit_project/BackQuizValiitProjectApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_quiz_valiit_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackQuizValiitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackQuizValiitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/valiit/back_quiz_valiit_project/BackQuizValiitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_quiz_valiit_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackQuizValiitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
