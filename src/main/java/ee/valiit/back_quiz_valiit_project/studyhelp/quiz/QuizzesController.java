13b38f69d640d724044428d7814eede022d1fa53
asyawind <berestovaja@gmail.com>
2023-02-10 10:20:53 +0200

    refactored class names

--- src/main/java/ee/valiit/back_quiz_valiit_project/studyhelp/quiz/QuizzesController.java
+++ src/main/java/ee/valiit/back_quiz_valiit_project/studyhelp/quiz/QuizzesController.java
@@ -9,21 +9,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/quiz")
 public class QuizzesController {
 
     @Resource
     private QuizzesService quizzesService;
 
 
-    @PostMapping()
+    @PostMapping("/quiz")
     @Operation(summary = "Create new Quiz", description = "Posts new quiz name and answers needed, and returs quizName and quizId")
     public QuizResponse addNewQuiz(@RequestParam Integer userId, @RequestBody QuizRequest quizRequest) {
         return quizzesService.addNewQuiz(userId, quizRequest);
     }
 
 
-    @GetMapping("/user")
+    @GetMapping("menu/user")
     @Operation(summary = "Finds all personal quizzes", description = "Finds all quizzes in quiz table by userId and quiz status")
     public List<QuizDto> getUserQuizzes(@RequestParam Integer userId) {
         List<QuizDto> quizzes = quizzesService.getUserQuizzes(userId);
@@ -31,7 +30,7 @@ public class QuizzesController {
     }
 
 
-    @GetMapping("/public")
+    @GetMapping("menu/public")
     @Operation(summary = "Finds all public quizzes", description = "Finds all active public quizzes in quiz table")
     public List<QuizDto> getPublicQuizzes() {
         List<QuizDto> quizzes = quizzesService.getPublicQuizzes();
