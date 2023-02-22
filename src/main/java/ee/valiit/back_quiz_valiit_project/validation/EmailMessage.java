8ff8545e0acfb00a38cf76460647b56a883acda1
Kivitagapeidus <S88rumail@gmail.com>
2023-02-22 16:39:56 +0200

    AnswerMapper text empty string to null

--- src/main/java/ee/valiit/back_quiz_valiit_project/validation/EmailMessage.java
+++ src/main/java/ee/valiit/back_quiz_valiit_project/validation/EmailMessage.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 @Getter
 public enum EmailMessage {
 
-    EMAIL_TO_ADMIN("Studyhelp uus kasutaja!!!", "Uus kasutaja vajab valideerimist.\n http://localhost:8081/#/login"),
-    EMAIL_TO_USER_APPROVED("Studyhelp kasutaja aktiveeritud", "Teie õpetaja konto on aktiveeritud. \n http://localhost:8081/#/login"),
+    EMAIL_TO_ADMIN("Studyhelp uus kasutaja!!!", "Uus kasutaja vajab valideerimist.\n http://localhost:8081/#/"),
+    EMAIL_TO_USER_APPROVED("Studyhelp kasutaja aktiveeritud", "Teie õpetaja konto on aktiveeritud. \n http://localhost:8081/#/"),
     EMAIL_TO_USER_PENDING("Studyhelp vajab faile", "Teie Studyhelp konto aktiveerimiseks palun saada koopia oma Id-kaardist aadressile noreplymailtestservice@gmail.com");
 
 
